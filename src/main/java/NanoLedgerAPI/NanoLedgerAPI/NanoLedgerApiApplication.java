af22c34a94696e99208b61b9224f201f847633db
Jehison Bustamante <85035415+JehisonBustamante@users.noreply.github.com>
2026-03-10 16:07:17 -0300

    Initial commit: NanoLedger API structure

---
+++ src/main/java/NanoLedgerAPI/NanoLedgerAPI/NanoLedgerApiApplication.java
@@ -0,0 +1,13 @@
+package NanoLedgerAPI.NanoLedgerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoLedgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoLedgerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NanoLedgerAPI/NanoLedgerAPI/NanoLedgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package NanoLedgerAPI.NanoLedgerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoLedgerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
